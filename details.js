44df521c5be2b8a4228ba839d7c39424fdce6f14
vero896 <verogonza896@gmail.com>
2023-10-29 23:57:55 -0500

    arreglo details

--- details.js
+++ details.js
@@ -5,31 +5,40 @@ let eventSearch = eventsDetails.find(search => search._id == id)
 const contenedorDetails = document.getElementById("contenedorDetails")
 
 function paintCards(arrayCard) {
-    
 
-      const card = document.createElement("div")
 
-      card.classList.add("card")
-      card.style.width = "25rem "
-      card.innerHTML = `
-      <img src="${arrayCard.image} alt="...">
-       
-      <div class="card-body bg-dark-subtle text-emphasis-ligth border border-dark rounded-bottom p-3 " >
-        <h5 class="card-title text-center"> ${arrayCard.name}</h5>
-     <p class="card-text">Date: ${arrayCard.date}</p>
-     <p class="card-text">${arrayCard.description}</p>
-      <p class="card-text">Category:${arrayCard.category}</p>
-     <p class="card-text">Place:${arrayCard.place}</p>
-      <p class="card-text">Capacity: ${arrayCard.capacity}</p>
-       
-      <p class="card-text">Price:${arrayCard.price}</p>
-     <p class="card-text">Estimate:${arrayCard.estimate}</p> 
+  const card = document.createElement("div")
+
+  card.classList.add("card")
+  card.style.width = "25rem "
+  card.innerHTML = `
+  
+      <div class=" card mb-3 style= max-width: 540px;">
+  
+   
+       <img src=${arrayCard.image} style="max-width: 300px class = card-img h-100 img-fluid alt="...">
+       </div>
+      
+      <div class="card-body  bg-dark-subtle text-emphasis-ligth border border-dark rounded-bottom p-3 " >
+          <h5 class="card-title text-center"> ${arrayCard.name}</h5>
+          <p class="card-text">Date: ${arrayCard.date}</p>
+         <p class="card-text">${arrayCard.description}</p>
+        <p class="card-text">Category:${arrayCard.category}</p>
+        <p class="card-text">Place:${arrayCard.place}</p>
+        <p class="card-text">Capacity: ${arrayCard.capacity}</p>
+        <p class="card-text"> ${arrayCard.assistance == null ? "Estimate: " + arrayCard.estimate : "Assistance: " + arrayCard.assistance}</p>
+        <p class="card-text">Price:${arrayCard.price}</p>
+        </div>
+        
+  
+
   `
-      contenedorDetails.appendChild(card)
-  }
+  contenedorDetails.appendChild(card)
+}
 
 
 paintCards(eventSearch)
+
 
 
 
